01ee10fbc0ceb04575e643b1182795fbbb965734
cjasicki <cjasicki@gmail.com>
2020-01-19 22:46:59 -0600

    Add project files.

---
+++ Example6_CS/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Example6_CS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvDisplay = new System.Windows.Forms.DataGridView();
+            this.lblError = new System.Windows.Forms.Label();
+            this.dgvInfo = new System.Windows.Forms.DataGridView();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.sfdXML = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDisplay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInfo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvDisplay
+            // 
+            this.dgvDisplay.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDisplay.Location = new System.Drawing.Point(50, 350);
+            this.dgvDisplay.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dgvDisplay.Name = "dgvDisplay";
+            this.dgvDisplay.RowHeadersWidth = 51;
+            this.dgvDisplay.RowTemplate.Height = 24;
+            this.dgvDisplay.Size = new System.Drawing.Size(700, 150);
+            this.dgvDisplay.TabIndex = 3;
+            // 
+            // lblError
+            // 
+            this.lblError.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(50, 520);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(700, 23);
+            this.lblError.TabIndex = 4;
+            this.lblError.Text = "(error)";
+            // 
+            // dgvInfo
+            // 
+            this.dgvInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvInfo.Location = new System.Drawing.Point(50, 175);
+            this.dgvInfo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dgvInfo.Name = "dgvInfo";
+            this.dgvInfo.RowHeadersWidth = 51;
+            this.dgvInfo.RowTemplate.Height = 24;
+            this.dgvInfo.Size = new System.Drawing.Size(700, 150);
+            this.dgvInfo.TabIndex = 2;
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(50, 125);
+            this.btnExport.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(75, 23);
+            this.btnExport.TabIndex = 1;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.BtnExport_Click_1);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(200, 50);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(400, 39);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "FileDialog and XML";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.dgvDisplay);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.dgvInfo);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "Form1";
+            this.Text = "FileDialog and XML";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDisplay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInfo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.DataGridView dgvDisplay;
+        internal System.Windows.Forms.Label lblError;
+        internal System.Windows.Forms.DataGridView dgvInfo;
+        internal System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.SaveFileDialog sfdXML;
+        private System.Windows.Forms.Button btnExport;
+    }
+}
+
---
+++ Example6_CS/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Example6_CS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //*****
+        //** Global variable
+        //*****
+        DataSet dsData = null;
+
+        //*****
+        //** Form
+        //*****
+        //*******************************************************
+        //** Procedure: Form1_Load()
+        //**   Initializes datagridview
+        //*******************************************************
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblError.Text = "";
+            LoadPayrollInfo();
+        }
+
+        //****
+        //** Button
+        //** Procedure:btnExport_load()
+        //** Exports dataset to XML file
+
+        private void BtnExport_Click_1(object sender, EventArgs e)
+        {
+            DialogResult dlgAnswer;
+            DataSet dsXML;
+
+            sfdXML.DefaultExt = "xml";
+            sfdXML.Filter = "XML files(*.xml)|*.xml|All files(*.*|*.*";
+            sfdXML.InitialDirectory = "C:\\Users\\cjasi\\Documents\\";
+            sfdXML.OverwritePrompt = true;
+            sfdXML.Title = "Save XML File";
+
+            dlgAnswer = sfdXML.ShowDialog();
+            if (dlgAnswer == DialogResult.OK)
+            {
+                dsData.WriteXml(sfdXML.FileName);
+
+                // Now read back into another dataset
+                dsXML = new DataSet();
+                dsXML.ReadXml(sfdXML.FileName);
+                dgvDisplay.DataSource = dsXML.Tables[0];
+            }
+        }
+
+        //*****
+        //** Procedure
+        //*****
+        //*******************************************************
+        //** Procedure: LoadPayrollInfo()
+        //**   Displays contents of Payroll table
+        //*******************************************************
+        private void LoadPayrollInfo()
+        {
+            SqlConnection cnSQL;
+            SqlDataAdapter daSQL;
+
+            lblError.Text = "";
+            btnExport.Enabled = false;
+
+            if (ConfigurationManager.ConnectionStrings["Payroll"] == null)
+            {
+                lblError.Text = "No Connection to Database";
+            }
+            else
+            {
+                cnSQL = clsDatabase.AcquireConnection("Payroll");
+                if(cnSQL == null)
+                {
+                    lblError.Text = "Error Connection to Database";
+                }
+                else
+                {
+                    // Remove any existion dataset
+                    if(dsData != null)
+                    {
+                        dsData.Dispose();
+                    }
+                    dsData = new DataSet();
+                    try
+                    {
+                        daSQL = new SqlDataAdapter("SELECT * FROM dbo.Payroll;", cnSQL);
+                        daSQL.Fill(dsData);
+                        daSQL.Dispose();
+                        dgvInfo.DataSource = dsData.Tables[0];
+                        btnExport.Enabled = true;
+                    }
+                    catch
+                    {
+                        lblError.Text = "Error retrieving Payroll data";
+                        dsData.Dispose();
+                        dsData = null;
+                    }
+                    finally
+                    {
+                        cnSQL.Close();
+                        cnSQL.Dispose();
+                    }
+                }
+            }
+        }
+    }
+}
